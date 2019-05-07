c3b889ef51a22cd5c821cd2d9fb757a787012456
Julian <julian@juliantomlindesign.com>
2019-05-07 19:40:47 +0000

    passed props into username and messages components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,35 @@ import Chatbar from './ChatBar.jsx'
 import Messages from './Message.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+                  currentUser: {name: "Bob"},
+                  messages: [
+                    {
+                      username: "Bob",
+                      content: "Has anyone seen my marbles?",
+                      id: 1,
+                    },
+                    {
+                      username: "Anonymous",
+                      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+                      id: 2,
+                    }
+                  ]
+                }
+
+
+
+  }
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <Messages />
-        <Chatbar />
+        <Messages message={this.state.messages}/>
+        <Chatbar username={this.state.currentUser.name}/>
       </div>
     );
   }
--- src/Message.jsx
+++ src/Message.jsx
@@ -4,13 +4,19 @@ class Messages extends Component {
   render() {
     return (
       <main className="messages">
-        <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
-        </div>
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
+        {this.props.message.map(message => {
+          return (
+              <div className="message" key={message.id}>
+                <span className="message-username">{message.username}</span>
+                <span className="message-content">{message.content}</span>
+              </div>
+              //<div className="message system">
+              //  Anonymous1 changed their name to nomnom.
+              //</div>
+            )
+        })
+      }
+
       </main>
     )
   }
